9fff8ff08ec3eaa48738c08f39120c3ecc4516fc
Mytherin <mark.raasveldt@gmail.com>
2025-03-03 19:00:33 +0100

    More tables

--- src/ducklake_initializer.cpp
+++ src/ducklake_initializer.cpp
@@ -30,8 +30,27 @@ void DuckLakeInitializer::InitializeNewDuckLake() {
 		throw InvalidInputException("Attempting to create a new ducklake instance but data_path is not set - set the DATA_PATH parameter to the desired location of the data files");
 	}
 	// FIXME: create schema if not exists/check schema
+	// FIXME: if any of the tables exist - fail
 	// initialize the ducklake tables
 	auto &table = CreateTable("ducklake_info", {"data_path"}, {LogicalType::VARCHAR});
+	CreateTable("ducklake_snapshot", {"snapshot_id", "snapshot_time"}, {LogicalType::BIGINT, LogicalType::TIMESTAMP});
+	CreateTable("ducklake_schema", {"schema_id", "schema_uuid", "begin_snapshot", "end_snapshot", "schema_name"}, {LogicalType::BIGINT, LogicalType::UUID, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::VARCHAR});
+	CreateTable("ducklake_table", {"table_id", "table_uuid", "begin_snapshot", "end_snapshot", "schema_id", "table_name"}, {LogicalType::BIGINT, LogicalType::UUID, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::VARCHAR});
+	CreateTable("ducklake_column", {"column_id", "begin_snapshot", "end_snapshot", "table_id", "column_order", "column_name", "column_type", "default_value"}, {LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR});
+	CreateTable("ducklake_data_file", {"data_file_id", "begin_snapshot", "end_snapshot", "table_id", "file_order", "path", "file_format", "record_count", "file_size_bytes", "partition_id"}, {
+	LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT});
+	CreateTable("ducklake_delete_file", {"delete_file_id", "begin_snapshot", "end_snapshot", "data_file_id", "path", "delete_count", "file_size_bytes"}, {
+	LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::VARCHAR, LogicalType::BIGINT, LogicalType::BIGINT});
+//	CreateTable("ducklake_partition_info", {}, {});
+//	CreateTable("ducklake_partition_column_info", {}, {});
+//	CreateTable("ducklake_partition_column_transforms", {}, {});
+//	CreateTable("ducklake_column_statistics", {}, {});
+//	CreateTable("ducklake_sorting_info", {}, {});
+//	CreateTable("ducklake_sorting_column_info", {}, {});
+//	CreateTable("ducklake_view", {}, {});
+//	CreateTable("ducklake_macro", {}, {});
+
+
 	DataChunk data_path_chunk;
 	vector<LogicalType> data_path_types = {LogicalType::VARCHAR};
 	data_path_chunk.Initialize(context, data_path_types);
