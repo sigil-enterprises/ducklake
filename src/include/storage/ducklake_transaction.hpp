c9fe1fdefc32588aa23af5ce71cbb11fcd7eff1b
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2025-07-29 03:50:42 +0700

    Update ducklake_transaction.hpp

--- src/include/storage/ducklake_transaction.hpp
+++ src/include/storage/ducklake_transaction.hpp
@@ -193,7 +193,6 @@ private:
 	case_insensitive_map_t<unique_ptr<DuckLakeCatalogSet>> new_tables;
 	set<TableIndex> dropped_tables;
 	set<TableIndex> renamed_tables;
-	set<TableIndex> real_dropped_tables;
 	set<TableIndex> dropped_views;
 	unordered_map<string, DataFileIndex> dropped_files;
 	set<TableIndex> tables_deleted_from;
