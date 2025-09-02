448a4927225be17a0e0a3705ee89b7aa1aac5f2e
pdet <pedroholanda@gmail.com>
2025-09-02 14:00:56 +0200

    Add sqlite here

--- extension_config.cmake
+++ extension_config.cmake
@@ -9,5 +9,12 @@ duckdb_extension_load(ducklake
 duckdb_extension_load(icu)
 duckdb_extension_load(json)
 duckdb_extension_load(tpch)
+
+duckdb_extension_load(sqlite_scanner
+        ${STATIC_LINK_SQLITE} LOAD_TESTS
+        GIT_URL https://github.com/duckdb/duckdb-sqlite
+        GIT_TAG ed38d770e0bbf1d5a6660ec1887cc5abef65be15
+        APPLY_PATCHES
+        )
 # Any extra extensions that should be built
 # e.g.: duckdb_extension_load(json)
