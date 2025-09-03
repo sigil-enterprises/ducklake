1f7f0aa2d94af8bd5b48967f964e0a34465119be
pdet <pedroholanda@gmail.com>
2025-09-03 11:14:36 +0200

    All the pieces to get sqlite to run

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ EXT_CONFIG=${PROJ_DIR}extension_config.cmake
 
 # Include the Makefile from extension-ci-tools
 include extension-ci-tools/makefiles/duckdb_extension.Makefile
+
+# We have to overwrite the test_release_internal from extension-ci-tools/makefiles/duckdb_extension.Makefile to run all
+# Of our catalog shebang
+test_release_internal:
+	./build/release/$(TEST_PATH) "test/*" --test-config test_config/sqlite.json
+	./build/release/$(TEST_PATH) "test/*"
--- extension_config.cmake
+++ extension_config.cmake
@@ -11,13 +11,15 @@ duckdb_extension_load(json)
 duckdb_extension_load(tpch)
 
 duckdb_extension_load(sqlite_scanner
+        DONT_LINK
+        APPLY_PATCHES
         GIT_URL https://github.com/duckdb/duckdb-sqlite
-        GIT_TAG ed38d770e0bbf1d5a6660ec1887cc5abef65be15
+        GIT_TAG 801922e968dd9b72b055ed0f9857cd7421200e6f
 )
 
-#duckdb_extension_load(postgres_scanner
-#            GIT_URL https://github.com/duckdb/duckdb-postgres
-#            GIT_TAG e58cd1dfed98a04ce3e928d5b941e5e2c533ba12
-#)
-# Any extra extensions that should be built
-# e.g.: duckdb_extension_load(json)
+duckdb_extension_load(postgres_scanner
+        DONT_LINK
+        APPLY_PATCHES
+        GIT_URL https://github.com/duckdb/duckdb-postgres
+        GIT_TAG e58cd1dfed98a04ce3e928d5b941e5e2c533ba12
+)
