5aec4b06eee1aef4b0dc18eb28b516d147a0f77a
pdet <pedroholanda@gmail.com>
2025-09-05 12:34:17 +0200

    Changing to Mark's machine

--- src/storage/ducklake_checkpoint.cpp
+++ src/storage/ducklake_checkpoint.cpp
@@ -9,14 +9,31 @@
 namespace duckdb {
 
 void DuckLakeTransactionManager::Checkpoint(ClientContext &context, bool force) {
-	if (!context.transaction.IsAutoCommit()) {
-		throw InvalidConfigurationException("CHECKPOINT; is only allowed with auto_commit = true");
+	auto &crazy = DuckLakeTransaction::Get(context,ducklake_catalog);
+	if (crazy.ChangesMade()) {
+		throw InternalException("bla");
 	}
+	// auto &conn = crazy.GetConnection();
+	// if (context.transaction.ActiveTransaction().) {
+	// 	throw InvalidConfigurationException("CHECKPOINT can't be called in a begin transaction.");
+	// }
+	// ducklake_catalog.db
+	// auto &duck_transaction = context.transaction.ActiveTransaction().GetTransaction()
+	// 		if (duck_transaction.ChangesMade()) {
+	// 			throw TransactionException("Cannot CHECKPOINT: the current transaction has transaction local changes");
+	// 		}
 	// We start a new connection
 	auto conn = make_uniq<Connection>(ducklake_catalog.GetDatabase());
-	auto checkpoint_query = StringUtil::Replace(DUCKLAKE_CHECKPOINT_QUERY, "{CATALOG}",
+	auto checkpoint_query = StringUtil::Replace(DUCKLAKE_CHECKPOINT_MODIFICATIONS, "{CATALOG}",
 	                                            KeywordHelper::WriteQuoted(ducklake_catalog.GetName(), '\''));
 	auto res = conn->Query(checkpoint_query);
+	if (res->HasError()) {
+		res->GetErrorObject().Throw("Failed to perform CHECKPOINT; in DuckLake:  ");
+	}
+
+	checkpoint_query = StringUtil::Replace(DUCKLAKE_CHECKPOINT_COMPACTION, "{CATALOG}",
+	                                            KeywordHelper::WriteQuoted(ducklake_catalog.GetName(), '\''));
+	res = conn->Query(checkpoint_query);
 	if (res->HasError()) {
 		res->GetErrorObject().Throw("Failed to perform CHECKPOINT; in DuckLake:  ");
 	}
