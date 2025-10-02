78c029c855bff2932047b042041eda685d6e20a7
pdet <pedroholanda@gmail.com>
2025-10-02 15:40:06 +0200

    [BUG] Properly check hive paths on multikeys

--- src/functions/ducklake_compaction_functions.cpp
+++ src/functions/ducklake_compaction_functions.cpp
@@ -381,6 +381,9 @@ DuckLakeCompactor::GenerateCompactionCommand(vector<DuckLakeCompactionFileEntry>
 			// This means we have a hive partition.
 			for (idx_t i = 0; i < path_result.size() - 1; i++) {
 				data_path += path_result[i];
+				if (i != path_result.size() - 2) {
+					data_path += catalog.Separator();
+				}
 			}
 			// If we do have a hive partition, let's verify all files have the same one.
 			for (idx_t i = 1; i < actionable_source_files.size(); i++) {
