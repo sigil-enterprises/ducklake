46ee764d5b564b46b5a5f65102cb11127bc11d50
dentinyhao <dentinyhao@gmail.com>
2026-05-05 23:57:48 -0700

    Fix table/scalar macro with same name creation in same txn

--- src/storage/ducklake_schema_entry.cpp
+++ src/storage/ducklake_schema_entry.cpp
@@ -408,7 +408,8 @@ void DuckLakeSchemaEntry::AddEntry(CatalogType type, unique_ptr<CatalogEntry> en
 
 void DuckLakeSchemaEntry::TryDropSchema(DuckLakeTransaction &transaction, bool cascade) {
 	auto local_tables = transaction.GetTransactionLocalEntries(CatalogType::TABLE_ENTRY, name);
-	auto local_macros = transaction.GetTransactionLocalEntries(CatalogType::MACRO_ENTRY, name);
+	auto local_scalar_macros = transaction.GetTransactionLocalEntries(CatalogType::MACRO_ENTRY, name);
+	auto local_table_macros = transaction.GetTransactionLocalEntries(CatalogType::TABLE_MACRO_ENTRY, name);
 	if (!cascade) {
 		// get a list of all dependents
 		vector<reference<CatalogEntry>> dependents;
@@ -482,9 +483,15 @@ void DuckLakeSchemaEntry::TryDropSchema(DuckLakeTransaction &transaction, bool c
 				dependents.push_back(*entry.second);
 			}
 		}
-		if (local_macros) {
-			dependents.reserve(dependents.size() + local_macros->GetEntries().size());
-			for (auto &entry : local_macros->GetEntries()) {
+		if (local_scalar_macros) {
+			dependents.reserve(dependents.size() + local_scalar_macros->GetEntries().size());
+			for (auto &entry : local_scalar_macros->GetEntries()) {
+				dependents.push_back(*entry.second);
+			}
+		}
+		if (local_table_macros) {
+			dependents.reserve(dependents.size() + local_table_macros->GetEntries().size());
+			for (auto &entry : local_table_macros->GetEntries()) {
 				dependents.push_back(*entry.second);
 			}
 		}
@@ -509,9 +516,15 @@ void DuckLakeSchemaEntry::TryDropSchema(DuckLakeTransaction &transaction, bool c
 			local_entries_to_drop.push_back(*entry.second);
 		}
 	}
-	if (local_macros) {
-		local_entries_to_drop.reserve(local_entries_to_drop.size() + local_macros->GetEntries().size());
-		for (auto &entry : local_macros->GetEntries()) {
+	if (local_scalar_macros) {
+		local_entries_to_drop.reserve(local_entries_to_drop.size() + local_scalar_macros->GetEntries().size());
+		for (auto &entry : local_scalar_macros->GetEntries()) {
+			local_entries_to_drop.push_back(*entry.second);
+		}
+	}
+	if (local_table_macros) {
+		local_entries_to_drop.reserve(local_entries_to_drop.size() + local_table_macros->GetEntries().size());
+		for (auto &entry : local_table_macros->GetEntries()) {
 			local_entries_to_drop.push_back(*entry.second);
 		}
 	}
