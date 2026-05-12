b9ff120da17d0cfdf94f2157ba5864dc8e932548
Pedro Holanda <pedroholanda@gmail.com>
2026-05-12 15:02:21 +0200

    Quack extension_config to https://github.com/duckdb/duckdb-quack.git

--- extension_config.cmake
+++ extension_config.cmake
@@ -5,19 +5,6 @@ duckdb_extension_load(ducklake
         SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
 )
 
-if($ENV{ENABLE_QUACK})
-    include_directories(
-            ${CMAKE_CURRENT_LIST_DIR}/duckdb/third_party/httplib
-            ${CMAKE_CURRENT_LIST_DIR}/duckdb/extension/autocomplete/include
-    )
-
-    duckdb_extension_load(quack
-            LOAD_TESTS
-            GIT_URL git@github.com:pdet/duckdb-quack.git
-            GIT_TAG d1870ae6b95c1a903c0d97548d637096a4bf789a
-    )
-endif()
-
 if(NOT DEFINED ENV{DISABLE_EXTENSIONS_FOR_TEST})
     duckdb_extension_load(icu)
     duckdb_extension_load(json)
@@ -31,4 +18,16 @@ endif()
 
 if($ENV{ENABLE_POSTGRES_SCANNER})
     include("${EXTENSION_CONFIG_BASE_DIR}/postgres_scanner.cmake")
+endif()
+
+if($ENV{ENABLE_QUACK})
+    include_directories(
+            ${CMAKE_CURRENT_LIST_DIR}/duckdb/third_party/httplib
+            ${CMAKE_CURRENT_LIST_DIR}/duckdb/extension/autocomplete/include
+    )
+    duckdb_extension_load(quack
+            LOAD_TESTS
+            GIT_URL https://github.com/duckdb/duckdb-quack.git
+            GIT_TAG be9c9bb5bd703f5e6d7e08a47de89eeb4aa51a1b
+    )
 endif()
